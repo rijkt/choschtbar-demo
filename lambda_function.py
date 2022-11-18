d75516ece8d8be84f730ed31bed65c218b42d74c
Pascal Ellenberger <pascal.ellenberger@protonmail.com>
2022-11-18 16:50:57 +0100

    fix cors error on error response

--- lambda_function.py
+++ lambda_function.py
@@ -1,19 +1,25 @@
-import json
 import boto3
+import json
 import os
 
 def lambda_handler(event, context):
     required_pw = os.environ['PASSWORD']
-    body = json.loads(event['body']) # todo: directly send JSON, don't stringify
+    body = json.loads(event['body'])  # todo: directly send JSON, don't stringify
     key = body['key']
     pw = body['pw']
     if (required_pw != pw):
-        raise Exception('Password does not match')
+        return {
+            'statusCode': 401,
+            'body': 'Wrong password',
+            'headers': {
+                'Access-Control-Allow-Origin': '*'
+            }
+        }
     url = boto3.client('s3').generate_presigned_url(
-    ClientMethod='put_object', 
-    Params={'Bucket': 'choschtbar-touren', 'Key': key, 'ACL': 'public-read'},
-    ExpiresIn=3600)  # will this work in summer time?
-    
+        ClientMethod='put_object',
+        Params={'Bucket': 'choschtbar-touren', 'Key': key, 'ACL': 'public-read'},
+        ExpiresIn=3600)
+
     return {
         'statusCode': 200,
         'body': json.dumps(url),
